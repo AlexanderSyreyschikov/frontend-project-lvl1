a53e7e6e09023718de3a2b8e1c57fff51046e1b0
Александр Сырейщиков <azonus1993@gmail.com>
2021-02-27 19:23:31 +0300

    rename function gameData to getGameData and rename gameInit to initGame

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,10 +3,10 @@ import init from '../index.js';
 
 const isEven = (number) => number % 2 === 0;
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const gameData = () => {
+const getGameData = () => {
   const question = getRandomNumber();
   const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
-const gameInit = () => init(gameData, description);
-export default gameInit;
+const initGame = () => init(getGameData, description);
+export default initGame;
