b9cf60ec0922319120b4834315ae3314eea116b0
Александр Сырейщиков <azonus1993@gmail.com>
2021-03-07 17:33:02 +0300

    removing magic numbers to game brain-progression

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -4,8 +4,10 @@ import init from '../index.js';
 const description = 'What number is missing in the progression?';
 
 const getCollProgression = (lengthColl) => {
+  const stepMinValue = -15;
+  const stepMaxValue = 15;
   const start = getRandomNumber();
-  const step = getRandomNumber(-15, 15);
+  const step = getRandomNumber(stepMinValue, stepMaxValue);
   const coll = [start];
   for (let i = 2; i <= lengthColl; i += 1) {
     coll.push(start + (i - 1) * step);
@@ -14,7 +16,9 @@ const getCollProgression = (lengthColl) => {
 };
 
 const getGameData = () => {
-  const lengthProgression = getRandomNumber(5, 10);
+  const minLengthProgression = 5;
+  const maxLengthProgression = 10;
+  const lengthProgression = getRandomNumber(minLengthProgression, maxLengthProgression);
   const positionHidden = getRandomNumber(0, (lengthProgression - 1));
   const collProgression = getCollProgression(lengthProgression);
   let answer = collProgression[positionHidden];
